4d77f1c0b17a39eac450b1d75400ed04c94e6bb0
Apurva-wanjari19 <apurvawanjari175@gmail.com>
2025-06-18 16:39:20 +0530

    Delete hide_pos_opencashbox directory

--- hide_pos_opencashbox/models/pos_config_inherit.py
+++
@@ -1,8 +0,0 @@
-from odoo import models, fields
-
-class PosConfig(models.Model):
-    _inherit = 'pos.config'
-
-    hide_opening_cash_control = fields.Boolean(
-        string="Hide Opening Cash Control"
-    )
